2e2de99ed9bd081b356351a0f13f5bc3141f7729
Aditya Jadhav <39948420+Adityaaj18@users.noreply.github.com>
2022-02-09 13:43:32 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import Header from './components/Header'
+import {
+   BrowserRouter as Router,
+   Routes,
+   Route,
+   Navigate,
+   Link,
+   Outlet,
+   useParams,
+   NavLink,
+   useNavigate,
+   useLocation
+} from 'react-router-dom'
+import AboutUs from './components/AboutUs'
+import Services from './components/Services'
+import Projects from './components/Projects'
+import ContactUs from './components/ContactUs'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return (
+      <div className="App">
+         <Routes>
+            <Route path="/" element={<Header />} />
+            <Route path="/about" element={<AboutUs />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<ContactUs />} />
+         </Routes>
+      </div>
+   )
 }
 
-export default App;
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import logo from '../img/logo-white.png'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+   return (
+      <>
+         <Navbar
+            className="nav"
+            sticky="top"
+            expand="lg"
+            style={{ paddingTop: '0px', paddingBottom: '0px' }}
+         >
+            <Navbar.Brand className="logo-container">
+               <img src={logo} className="logo" style={{ marginTop: '15px' }} />
+            </Navbar.Brand>
+
+            <Navbar.Toggle className="hamburger" />
+            <Navbar.Collapse className="justify-content-end">
+               <Nav className="nav-link-container">
+                  <Nav.Link href="/">Home</Nav.Link>
+                  <Nav.Link href="/about">About Us</Nav.Link>
+                  <Nav.Link href="/services">Services</Nav.Link>
+                  <Nav.Link href="/projects">Projects</Nav.Link>
+                  <Nav.Link href="/contact">Contact Us</Nav.Link>
+               </Nav>
+            </Navbar.Collapse>
+         </Navbar>
+      </>
+   )
+}
+
+export default Header
