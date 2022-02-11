e007f1ba7701e7ba6e31b94d6e3b72eb3f3a8f71
Aditya Jadhav <39948420+Adityaaj18@users.noreply.github.com>
2022-02-11 13:28:52 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -17,12 +17,13 @@ import AboutUs from './pages/AboutUs'
 import Services from './pages/Services'
 import Projects from './pages/Projects'
 import ContactUs from './pages/ContactUs'
+import Home from './pages/Home'
 
 function App() {
    return (
       <div className="App">
          <Routes>
-            <Route path="/" element={<Header />} />
+            <Route path="/" element={<Home />} />
             <Route path="/about" element={<AboutUs />} />
             <Route path="/services" element={<Services />} />
             <Route path="/projects" element={<Projects />} />
---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Header from './Header'
+
+const Home = () => {
+   return (
+      <>
+         <Header />
+         <div className="main-section">
+            <div className="main-container">
+               <h1>
+                  Arkose Buildcom is your best homes, offices Architecture and
+                  Construction solution.
+               </h1>
+               <p>
+                  We provide overall planning, coordination and control of a
+                  project, so you don’t have to worry about anything. Our
+                  promise as a contractor is to build community value into every
+                  project while delivering professional expertise.
+               </p>
+            </div>
+         </div>
+      </>
+   )
+}
+
+export default Home
