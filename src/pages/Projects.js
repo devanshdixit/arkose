57e0ccbcdbccf93205e31e7f9f32c64b7a2ebd14
Aditya Jadhav <39948420+Adityaaj18@users.noreply.github.com>
2022-02-15 13:55:48 +0530

    gallery filter

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,6 +3,7 @@ import Header from './Header'
 import Footer from './Footer'
 import { FaHome } from 'react-icons/fa'
 import classNames from 'classnames'
+import AllData from './AllData'
 
 const Projects = () => {
    const activeTab = document.querySelector('.projects-category')
@@ -10,273 +11,106 @@ const Projects = () => {
    // activeTab.addEventListener('click', () => {
    //    activeTab.classList.toggle('active')
    // })
-   const [check1, setCheck1] = useState(false)
+   const [images, setImages] = useState(AllData)
+
+   const allItems = () => {
+      const finalData = AllData.filter((value) => {
+         return value
+      })
+
+      setImages(finalData)
+   }
+
+   const onlyArchitect = (categoryItem) => {
+      const finalData = AllData.filter((value) => {
+         return value.category == categoryItem
+      })
+      setImages(finalData)
+   }
+
+   const onlyInterior = (categoryItem) => {
+      const finalData = AllData.filter((value) => {
+         return value.category == categoryItem
+      })
+      setImages(finalData)
+   }
+
+   const onlyCivil = (categoryItem) => {
+      const finalData = AllData.filter((value) => {
+         return value.category == categoryItem
+      })
+      setImages(finalData)
+   }
+   const onlyPlanning = (categoryItem) => {
+      const finalData = AllData.filter((value) => {
+         return value.category == categoryItem
+      })
+      setImages(finalData)
+   }
+   const onlyRenovation = (categoryItem) => {
+      const finalData = AllData.filter((value) => {
+         return value.category == categoryItem
+      })
+      setImages(finalData)
+   }
 
    return (
       <>
          <div className="projects">
             <Header />
-            <input type="radio" name="photo" id="check1" checked />
-            <input
-               type="radio"
-               name="photo"
-               id="check2"
-               // onClick={() => {
-               //    setCheck1(!check1)
-               // }}
-            />
-            <input type="radio" name="photo" id="check3" />
-            <input type="radio" name="photo" id="check4" />
-            <input type="radio" name="photo" id="check5" />
-            <input type="radio" name="photo" id="check6" />
             <div className="about-us">
                <div className="about-us-container">
                   <h1>Our Projects</h1>
-
                   <p>
                      <FaHome className="home-icon" />
                      Home / projects
                   </p>
                </div>
             </div>
-            <div className="container">
-               <label for="check1" className="projects-category">
-                  All
-               </label>
-               <label for="check2" className="projects-category">
-                  Architechture
-               </label>
-               <label for="check3" className="projects-category">
-                  Interior
-               </label>
-               <label for="check4" className="projects-category">
-                  Construction
-               </label>
-               <label for="check5" className="projects-category">
-                  Project Planning
-               </label>
-               <label for="check6" className="">
-                  Renovation
-               </label>
+            <div className="container my-3">
+               <div className=" row proj-category">
+                  <div className="col-lg-2 col-md-4 col-sm-4 category-tab">
+                     <a onClick={allItems}>All</a>
+                  </div>
+                  <div className="col-lg-2 col-md-4 col-sm-4 category-tab">
+                     <a onClick={() => onlyArchitect('architechture')}>
+                        Architecture
+                     </a>
+                  </div>
+                  <div className="col-lg-2 col-md-4 col-sm-4 category-tab">
+                     <a onClick={() => onlyInterior('interior')}>Interior</a>
+                  </div>
+                  <div className="col-lg-2 col-md-4 col-sm-4 category-tab">
+                     <a onClick={() => onlyCivil('civil')}>Construction</a>
+                  </div>
+                  <div className="col-lg-2 col-md-4 col-sm-4 category-tab">
+                     <a onClick={() => onlyPlanning('planning')}>
+                        Project Planning
+                     </a>
+                  </div>
+                  <div className="col-lg-2 col-md-4 col-sm-4 category-tab">
+                     <a onClick={() => onlyRenovation('renovation')}>
+                        Renovation
+                     </a>
+                  </div>
+               </div>
             </div>
 
             <div className="gallery row">
-               <div
-                  className="gallery-img-wrapper col-xl-3 col-lg-4 col-md-6 col-sm-12 architecture"
-                  // {classNames(
-                  //    'gallery-img-wrapper col-xl-3 col-lg-4 col-md-6 col-sm-12 architecture',
-                  //    check1 ? 'd-block' : 'd-none'
-                  // )}
-               >
-                  <img
-                     className="gallery-img"
-                     src={require('../img/projects/project1.jpg')}
-                     alt=""
-                  />
-                  <div className="gallery-overlay">
-                     <a href="#">See More</a>
-                  </div>
-               </div>
-               <div className="gallery-img-wrapper col-xl-3 col-lg-4 col-md-6 col-sm-12 architecture">
-                  <img
-                     className="gallery-img"
-                     src={require('../img/projects/project2.jpg')}
-                     alt=""
-                  />
-                  <div className="gallery-overlay">
-                     <a href="#">See More</a>
-                  </div>
-               </div>
-               <div className="gallery-img-wrapper col-xl-3 col-lg-4 col-md-6 col-sm-12 architecture">
-                  <img
-                     className="gallery-img"
-                     src={require('../img/projects/project3.jpg')}
-                     alt=""
-                  />
-                  <div className="gallery-overlay">
-                     <a href="#">See More</a>
-                  </div>
-               </div>
-               <div className="gallery-img-wrapper col-xl-3 col-lg-4 col-md-6 col-sm-12 architecture">
-                  <img
-                     className="gallery-img"
-                     src={require('../img/projects/project4.jpg')}
-                     alt=""
-                  />
-                  <div className="gallery-overlay">
-                     <a href="#">See More</a>
-                  </div>
-               </div>
-               <div className="gallery-img-wrapper col-xl-3 col-lg-4 col-md-6 col-sm-12 architecture">
-                  <img
-                     className="gallery-img"
-                     src={require('../img/projects/project5.jpg')}
-                     alt=""
-                  />
-                  <div className="gallery-overlay">
-                     <a href="#">See More</a>
-                  </div>
-               </div>
-               <div className="gallery-img-wrapper col-xl-3 col-lg-4 col-md-6 col-sm-12 architecture">
-                  <img
-                     className="gallery-img"
-                     src={require('../img/projects/project6.jpg')}
-                     alt=""
-                  />
-                  <div className="gallery-overlay">
-                     <a href="#">See More</a>
-                  </div>
-               </div>
-               <div className="gallery-img-wrapper col-xl-3 col-lg-4 col-md-6 col-sm-12 civil">
-                  <img
-                     className="gallery-img"
-                     src={require('../img/projects/project7.jpg')}
-                     alt=""
-                  />
-                  <div className="gallery-overlay">
-                     <a href="#">See More</a>
-                  </div>
-               </div>
-               <div className="gallery-img-wrapper col-xl-3 col-lg-4 col-md-6 col-sm-12 civil">
-                  <img
-                     className="gallery-img"
-                     src={require('../img/projects/project8.jpg')}
-                     alt=""
-                  />
-                  <div className="gallery-overlay">
-                     <a href="#">See More</a>
-                  </div>
-               </div>
-               <div className="gallery-img-wrapper col-xl-3 col-lg-4 col-md-6 col-sm-12 civil">
-                  <img
-                     className="gallery-img"
-                     src={require('../img/projects/project9.jpg')}
-                     alt=""
-                  />
-                  <div className="gallery-overlay">
-                     <a href="#">See More</a>
-                  </div>
-               </div>
-               <div className="gallery-img-wrapper col-xl-3 col-lg-4 col-md-6 col-sm-12 interior">
-                  <img
-                     className="gallery-img"
-                     src={require('../img/projects/project10.jpg')}
-                     alt=""
-                  />
-                  <div className="gallery-overlay">
-                     <a href="#">See More</a>
-                  </div>
-               </div>
-               <div className="gallery-img-wrapper col-xl-3 col-lg-4 col-md-6 col-sm-12 interior">
-                  <img
-                     className="gallery-img"
-                     src={require('../img/projects/project11.jpg')}
-                     alt=""
-                  />
-                  <div className="gallery-overlay">
-                     <a href="#">See More</a>
-                  </div>
-               </div>
-               <div className="gallery-img-wrapper col-xl-3 col-lg-4 col-md-6 col-sm-12 interior">
-                  <img
-                     className="gallery-img"
-                     src={require('../img/projects/project12.jpg')}
-                     alt=""
-                  />
-                  <div className="gallery-overlay">
-                     <a href="#">See More</a>
-                  </div>
-               </div>
-               <div className="gallery-img-wrapper col-xl-3 col-lg-4 col-md-6 col-sm-12 interior">
-                  <img
-                     className="gallery-img"
-                     src={require('../img/projects/project13.jpg')}
-                     alt=""
-                  />
-                  <div className="gallery-overlay">
-                     <a href="#">See More</a>
-                  </div>
-               </div>
-               <div className="gallery-img-wrapper col-xl-3 col-lg-4 col-md-6 col-sm-12 interior">
-                  <img
-                     className="gallery-img"
-                     src={require('../img/projects/project14.jpg')}
-                     alt=""
-                  />
-                  <div className="gallery-overlay">
-                     <a href="#">See More</a>
-                  </div>
-               </div>
-               <div className="gallery-img-wrapper col-xl-3 col-lg-4 col-md-6 col-sm-12 interior">
-                  <img
-                     className="gallery-img"
-                     src={require('../img/projects/project15.jpg')}
-                     alt=""
-                  />
-                  <div className="gallery-overlay">
-                     <a href="#">See More</a>
-                  </div>
-               </div>
-               <div className="gallery-img-wrapper col-xl-3 col-lg-4 col-md-6 col-sm-12 planning">
-                  <img
-                     className="gallery-img"
-                     src={require('../img/projects/project16.jpg')}
-                     alt=""
-                  />
-                  <div className="gallery-overlay">
-                     <a href="#">See More</a>
-                  </div>
-               </div>
-               <div className="gallery-img-wrapper col-xl-3 col-lg-4 col-md-6 col-sm-12 planning">
-                  <img
-                     className="gallery-img"
-                     src={require('../img/projects/project17.jpg')}
-                     alt=""
-                  />
-                  <div className="gallery-overlay">
-                     <a href="#">See More</a>
-                  </div>
-               </div>
-               <div className="gallery-img-wrapper col-xl-3 col-lg-4 col-md-6 col-sm-12 planning">
-                  <img
-                     className="gallery-img"
-                     src={require('../img/projects/project18.jpg')}
-                     alt=""
-                  />
-                  <div className="gallery-overlay">
-                     <a href="#">See More</a>
-                  </div>
-               </div>
-               <div className="gallery-img-wrapper col-xl-3 col-lg-4 col-md-6 col-sm-12 renovation">
-                  <img
-                     className="gallery-img"
-                     src={require('../img/projects/project19.jpg')}
-                     alt=""
-                  />
-                  <div className="gallery-overlay">
-                     <a href="#">See More</a>
-                  </div>
-               </div>
-               <div className="gallery-img-wrapper col-xl-3 col-lg-4 col-md-6 col-sm-12 renovation">
-                  <img
-                     className="gallery-img"
-                     src={require('../img/projects/project20.jpg')}
-                     alt=""
-                  />
-                  <div className="gallery-overlay">
-                     <a href="#">See More</a>
-                  </div>
-               </div>
-               <div className="gallery-img-wrapper col-xl-3 col-lg-4 col-md-6 col-sm-12 renovation">
-                  <img
-                     className="gallery-img"
-                     src={require('../img/projects/project21.jpg')}
-                     alt=""
-                  />
-                  <div className="gallery-overlay">
-                     <a href="#">See More</a>
-                  </div>
-               </div>
+               {images.map((value) => {
+                  return (
+                     <div className="gallery-img-wrapper col-xl-3 col-lg-4 col-md-6 col-sm-12 ">
+                        <img
+                           className="gallery-img img-fluid"
+                           src={value.image}
+                           alt=""
+                        />
+                        <div className="gallery-overlay">
+                           <a href="#">See More</a>
+                        </div>
+                     </div>
+                  )
+               })}
             </div>
 
             <Footer />
